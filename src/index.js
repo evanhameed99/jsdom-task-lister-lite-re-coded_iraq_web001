e0e94d7e1a4dc0638b4c54540058c133fc8dd645
Evan hameed <evanhameed9966@gmail.com>
2020-05-19 13:30:20 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -1,13 +1,41 @@
 document.addEventListener("DOMContentLoaded", () => {
+
   // your code here
-let listUl = document.getElementById("tasks");
+  let taskDescription= document.getElementById('new-task-description');
+  const submit= document.getElementsByTagName('input')[1];
+  let tasksList=document.getElementById('tasks');
+  const form= document.getElementById('create-task-form');
+  let tasks=[];
 
-  document.querySelector("#btn-submit").addEventListener("click", function(event) {
-         event.preventDefault();
-         let inputText = document.getElementById("new-task-description").value;
-         let li = document.createElement("li");
-         li.innerText = inputText;
-         listUl.appendChild(li);
-}, false);
+  submit.addEventListener('click',function(event){
 
+    event.preventDefault();
+    if(taskDescription&& taskDescription.value){
+    let task ={
+
+      title: taskDescription
+    }
+    tasks.push(task);
+
+    let listItem=document.createElement('li');
+    let delBtn=document.createElement('button');
+
+    listItem.innerText= task.title.value;
+    delBtn.innerText= " x";
+
+    listItem.appendChild(delBtn);
+    tasksList.appendChild(listItem);
+
+    delBtn.onclick= function(){
+    tasksList.removeChild(listItem);
+    };
+
+    form.reset();
+
+  }
+  else{
+
+    alert("please type your task before submitting");
+  }
+  })
 });
